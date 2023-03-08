cfa2e523af8749e330ff2a81e5a962460c5e59c1
vathana <vath2503@gmail.com>
2023-03-08 23:40:27 +0700

    second commit

---
+++ src/components/Jobcard.vue
@@ -0,0 +1,122 @@
+<template lang="">
+    <a href="#">
+        <div class="block_card">
+            <div class="block_card_flex">
+                <div class="logo">
+                    <n-avatar data-logo
+                        size="medium"
+                        :src="card.logo"
+                    />
+                </div>
+                <div class="job_content">
+                    <div class="job_content_above">
+                        <h1>{{card.title}}</h1>
+                    </div>
+
+                    <div class="job_content_mid"  >
+                        <!-- item -->
+                        <div class="job_content_mid_item" v-for="item in card.postInfo">
+                            <n-avatar
+                                size="small"
+                                :src="item.icon"
+                            />
+                            <p>{{item.text}}</p>  
+                        </div>
+                    </div>
+
+                    <div class="job_content_under">
+                        <div v-for="(btn,key) in card.button" renderBtnColor>
+                            <n-button v-if="(key%2)==0" type="warning" style="height:2.3rem; border-radius:1.2rem;">
+                            {{btn}}
+                            </n-button>
+
+                            <n-button v-else type="error" style="height:2.3rem; border-radius:1.2rem;">
+                            {{btn}}
+                            </n-button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+</template>
+
+
+<script>
+export default {
+    props:['card'],
+}
+</script>
+
+
+<style scoped>
+
+  @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Inter:wght@200;300;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700&display=swap');
+  
+    /* -------------custom style naive's component---------------  */
+
+    /* logo */
+    .n-avatar[data-logo]{
+        width: 5rem;
+        height: 5rem;
+    }
+
+    /* -------------class binded------------ */
+    .color{
+        background: red;
+        color: black;
+    }
+
+    /* -------------general style----------- */
+    h1{
+        font-size: 2rem;
+        font-weight: 400;
+        text-align: start;
+    }
+    p{
+        font-size: 1.5rem;
+        font-weight: 400;
+        text-align: start;
+        color:#7090B0;
+    }
+    /* ---------card---------------- */
+    .block_card{
+        width:88rem;
+        height: 17rem;
+        border: 0.01px solid rgb(187, 185, 185);
+        padding: 3rem;
+        
+    }
+
+    .block_card_flex{
+        display: flex;
+        gap: 3rem;
+    }
+    /* ---------mid content ---------*/
+
+    .job_content_mid{
+        margin-top:2.4rem;
+        display: flex;
+        gap:2rem;
+        align-items: center;
+    }
+
+    .job_content_mid_item{
+        display: flex;
+        gap:1.5rem;
+    }
+
+    .n-avatar{
+        height: 2rem;
+        width: 2rem;
+    }
+
+    /* --------under content-------- */
+    .job_content_under{
+        margin-top:3rem;
+        display: flex;
+        gap:2.5rem;
+    }
+    
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,267 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+            <div class="heading">Find Your Dream Job</div>
+            <div class="filter">
+              <p>show 10 posts</p>
+              <n-space vertical>
+                <n-dropdown
+                  placement="bottom-start"
+                  trigger="click"
+                  size="small"
+                  :options="options"
+                  @select="handleSelect"
+                >
+                  <n-button>filter</n-button>
+                </n-dropdown>
+              </n-space>
+            </div>
+            <div v-for="(job,key) in card " :key="job.id">
+            <Jobcard :card="card[key]"/>
+            </div>
+            <n-space vertical>
+            <n-pagination v-model:page="page" :page-count="10" :page-slot="5" />
+            </n-space>
+    </div>
 </template>
 
-<script>
+<script >
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Jobcard from '@/components/Jobcard.vue'
+import { defineComponent } from "vue";
+import { useMessage } from "naive-ui";
+import { ref } from 'vue'
+
+const options = [
+  {
+    label: "Jay Gatsby",
+    key: "jay gatsby"
+  },
+  {
+    label: "Daisy Buchanan",
+    key: "daisy buchanan"
+  },
+  {
+    type: "divider",
+    key: "d1"
+  },
+  {
+    label: "Nick Carraway",
+    key: "nick carraway"
+  },
+  {
+    label: "Others",
+    key: "others1",
+  }
+];
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{
+    Jobcard,
+  },
+
+  data(){
+    return {
+      page: ref(2),
+      card:[
+        {
+          id:1,
+          logo: require("../assets/image/logo1.png"),
+          title: "Software Engineer (Android), Libraries",
+          postInfo:[
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "segment"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "time"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "salary"
+            }
+          ],
+          button:["fulltime","Private","urgent"]
+        },
+        {
+          id:2,
+          logo: require("../assets/image/logo1.png"),
+          title: "Software Engineer (Android), Libraries",
+          postInfo:[
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "segment"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "time"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "salary"
+            }
+          ],
+          button:["fulltime","Private","urgent"]
+        },
+        {
+          id:3,
+          logo: require("../assets/image/logo1.png"),
+          title: "Software Engineer (Android), Libraries",
+          postInfo:[
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "segment"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "time"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text:"salary"
+            }
+          ],
+          button:["fulltime","Private","urgent"]
+        },
+        {
+          id:3,
+          logo: require("../assets/image/logo1.png"),
+          title: "Software Engineer (Android), Libraries",
+          postInfo:[
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "segment"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "time"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text:"salary"
+            }
+          ],
+          button:["fulltime","Private","urgent"]
+        },
+        {
+          id:3,
+          logo: require("../assets/image/logo1.png"),
+          title: "Software Engineer (Android), Libraries",
+          postInfo:[
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "segment"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "time"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text:"salary"
+            }
+          ],
+          button:["fulltime","Private","urgent"]
+        },
+        {
+          id:3,
+          logo: require("../assets/image/logo1.png"),
+          title: "Software Engineer (Android), Libraries",
+          postInfo:[
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "segment"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "time"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text:"salary"
+            }
+          ],
+          button:["fulltime","Private","urgent"]
+        },
+        {
+          id:3,
+          logo: require("../assets/image/logo1.png"),
+          title: "Software Engineer (Android), Libraries",
+          postInfo:[
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "segment"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "time"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text:"salary"
+            }
+          ],
+          button:["fulltime","Private","urgent"]
+        },
+        {
+          id:3,
+          logo: require("../assets/image/logo1.png"),
+          title: "Software Engineer (Android), Libraries",
+          postInfo:[
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "segment"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text: "time"
+            },
+            {
+              icon: require("../assets/image/logo1.png"),
+              text:"salary"
+            }
+          ],
+          button:["fulltime","Private","urgent"]
+        }
+      ],  
+      options,
+      handleSelect(key) {
+        message.info(String(key));
+      }   
+    }
   }
 }
 </script>
+
+<style scoped>
+  .home{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap:2rem;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .heading{
+    margin-top: 100px ;
+        width: 88rem;
+        height: 200px;
+        border: 0.01px solid rgb(187, 185, 185);
+        padding: 3rem;
+        background-color: #EFF4FB;
+        font-size: 2.4rem;
+        text-align: center;
+  }
+  .filter{
+    width: 88rem;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .filter p{
+    font-size: 1.6rem ;
+    color: #7090B0;
+  }
+</style>
